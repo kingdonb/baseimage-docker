jedihe/baseimage-i386-docker

tag first version of yebyen/baseimage:i386

Makefile
@@ -1,5 +1,5 @@
-NAME = phusion/baseimage
-VERSION = 0.9.10
+NAME = yebyen/baseimage
+VERSION = i386
 
 .PHONY: all build test tag_latest release ssh
 

kingdonb/baseimage-docker

Makefile
@@ -1,5 +1,5 @@
-NAME = phusion/baseimage
-VERSION = 0.9.13
+NAME = yebyen/baseimage
+VERSION = armhf
 
 .PHONY: all build test tag_latest release ssh
 